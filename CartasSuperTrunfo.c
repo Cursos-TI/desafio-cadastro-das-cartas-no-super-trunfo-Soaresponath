9e2b545088cb0f3f0ef2c6b638d8b5737e89ea00
ROSIANE SOARES PONATH <202509031551@alunos.estacio.br>
2025-09-12 11:01:03 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    char carta1[04];
+    char estado1; // letra referente ao estado de A a H
+    char codigo1[10]; // codigo da carta 3 caracteres
+    char nomedaCidade1[50]; //nome da cidade com ate 20 caracteres
+    int populacao1; // população da cidade
+    float area1; // total da área da cidade em km²
+    float pib1; // pib total da cidade
+    int pontosTuristicos1; // número dos pontos turisticos da cidade
+
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Carta 1:");
+    scanf("%c",carta1);
+   
+    printf("Estado: ");
+    scanf("%c",&estado1);
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Código: ");
+    scanf("%s",&codigo1);
+
+    printf("Nome da cidade: ");
+    scanf("%s", &nomedaCidade1);
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos1);
 
     return 0;
+
+
 }
